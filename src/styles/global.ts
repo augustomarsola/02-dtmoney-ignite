5a9197b865b7fe08f49878cf2a089ae551d7039e
Augusto Marsola <augusto.marsola@live.com>
2022-03-16 23:24:55 -0300

    Add global style to App

---
+++ src/styles/global.ts
@@ -0,0 +1,46 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  :root {
+    --background: #f0f2f5;
+    --red: #e52e4d;
+    --blue: #5429DC;
+
+    --blue-light: #6933ff;
+
+    --text-title: #363f5f;
+    --text-boy: #969cb3;
+
+    --background: #f0f2f5;
+    --shape: #ffffff;
+  }
+
+  * {
+    margin: 0px;
+    padding: 0px;
+    box-sizing: border-box;
+  }
+
+  html {
+    @media (max-width: 1080px) {
+      font-size: 93.75%;
+    }
+    @media (max-width: 720px) {
+      font-size: 87.5%;
+    }
+  }
+
+  body {
+    background: var(--background);
+    -webkit-font-smoothing: antialiased;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  [disabled] {
+    opacity: 0.6;
+    cursor: not-allowed;
+  }
+`
