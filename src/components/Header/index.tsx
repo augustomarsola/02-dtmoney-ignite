88fe811485b271d0c452132c2fd898eb12d9ded1
Augusto Marsola <augusto.marsola@live.com>
2022-03-21 21:47:56 -0300

    🏠 Completed the react-modal implemetation

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,16 @@
 import logoImg from '../../assets/logo.svg'
 import { Container, Content } from './styles'
 
-export function Header() {
+interface HeaderProps {
+  onOpenNewTransactioModal: () => void;
+}
+
+export function Header({onOpenNewTransactioModal}: HeaderProps) {
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="Logo dt Money" />
-        <button type="button">
+        <button type="button" onClick={onOpenNewTransactioModal}>
           Nova transação
         </button>
       </Content>
