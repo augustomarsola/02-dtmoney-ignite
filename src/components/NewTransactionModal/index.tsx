0625fc8295408b6d39fb1719f8f3867e1bf82054
Augusto Marsola <augusto.marsola@live.com>
2022-03-21 22:32:53 -0300

    🎨 Init the custom style of the component

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,5 @@
 import Modal from "react-modal";
-import { Component } from "./styles";
+import { Container } from "./styles";
 
 interface NewTransactionalModalProps {
   isOpen: boolean;
@@ -13,8 +13,22 @@ export function NewTransactionalModal({ isOpen, onRequestClose }: NewTransaction
     <Modal
       isOpen={isOpen}
       onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
     >
-      <h2>Cadastrar transação</h2>
+      <Container>
+        <h2>Cadastrar transação</h2>
+
+        <input type="text" placeholder="Título" />
+
+        <input type="number" placeholder="Valor" />
+
+        <input type="text" placeholder="Categoria" />
+
+        <button type="submit">
+          Cadastrar
+        </button>
+      </Container>
     </Modal>
   );
 }
